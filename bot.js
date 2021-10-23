9ef8434ffebcc55a281f5be0953e78762853ea83
frostmint <90097027+frostmint@users.noreply.github.com>
2021-10-23 09:39:12 +0700

    discord bot

---
+++ bot.js
@@ -0,0 +1,21 @@
+/* install nodejs
+https://nodejs.org/en/
+
+use "npm install discord.js" first
+done
+*/
+
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+})
+
+client.login(process.env.TOKEN)
