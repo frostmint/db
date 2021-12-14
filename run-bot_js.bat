8a9a12e39a3a9d54e9b8fd44213f0113f0348672
DeezNut <90097027+frostmint@users.noreply.github.com>
2021-12-15 01:06:27 +0700

    Delete run-bot_js.bat

--- run-bot_js.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:main_file
-:: You know, if you want to run
-:: Javascript using nodejs
-:: You cannot double-click
-:: The file, so this batch-script enable to execute bot.js
-:: just double click this file
-cd /d %~dp0
-node bot.js
